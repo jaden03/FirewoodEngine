44663cf47c7cc24210207c56bf5517654c17f4e2
Jaden <jadendonald03@gmail.com>
2022-10-20 19:34:13 -0500

    Still working on audio

--- FirewoodEngine/Core/AudioManager.cs
+++ FirewoodEngine/Core/AudioManager.cs
@@ -1,65 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using OpenTK;
 using OpenTK.Audio.OpenAL;
+using OpenTK.Audio;
 
 namespace FirewoodEngine.Core
 {
     using static Logging;
     internal class AudioManager
     {
+        static readonly string filename = ("../../Sounds/pwwuitt.wav");
 
         public static void Init()
         {
             var device = Alc.OpenDevice(null);
-            var context = Alc.CreateContext(device, (int[])null);
 
+            var context = Alc.CreateContext(device, (int[])null);
             Alc.MakeContextCurrent(context);
+
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Audio Manager Initialized");
             Console.ForegroundColor = ConsoleColor.White;
 
 
-            int buffers, source;
-            AL.GenBuffers(1, out buffers);
-            AL.GenSources(1, out source);
+            int buffer, source, state;
+            buffer = AL.GenBuffer();
+            source = AL.GenSource();
 
-            int sampleFreq = 44100;
-            double dt = 2 * Math.PI / sampleFreq;
-            double amp = 0.5;
+            int channels, bits_per_sample, sample_rate;
+            byte[] sound_data = LoadWave(File.Open(filename, FileMode.Open), out channels, out bits_per_sample, out sample_rate);
 
-            int freq = 440;
-            var dataCount = sampleFreq / freq;
+            AL.BufferData(buffer, GetSoundFormat(channels, bits_per_sample), sound_data, sound_data.Length, sample_rate);
 
-            var sinData = new short[dataCount];
-            for (int i = 0; i < sinData.Length; ++i)
-            {
-                sinData[i] = (short)(amp * short.MaxValue * Math.Sin(i * dt * freq));
-            }
-            AL.BufferData(buffers, ALFormat.Mono16, sinData, sinData.Length, sampleFreq);
-            AL.Source(source, ALSourcei.Buffer, buffers);
-            AL.Source(source, ALSourceb.Looping, true);
-
+            AL.Source(source, ALSourcei.Buffer, buffer);
             AL.SourcePlay(source);
-            Console.ReadKey();
 
-            ///Dispose
-            if (context != ContextHandle.Zero)
+            do
             {
-                Alc.MakeContextCurrent(ContextHandle.Zero);
-                Alc.DestroyContext(context);
+                Thread.Sleep(250);
+                AL.GetSource(source, ALGetSourcei.SourceState, out state);
             }
-            context = ContextHandle.Zero;
+            while ((ALSourceState)state == ALSourceState.Playing);
 
-            if (device != IntPtr.Zero)
-            {
-                Alc.CloseDevice(device);
-            }
-            device = IntPtr.Zero;
+            AL.SourceStop(source);
+            AL.DeleteSource(source);
+            AL.DeleteBuffer(buffer);
         }
+
+        public static byte[] LoadWave(Stream stream, out int channels, out int bits, out int rate)
+        {
+            if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                // RIFF header
+                string signature = new string(reader.ReadChars(4));
+                if (signature != "RIFF")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                int riff_chunck_size = reader.ReadInt32();
+
+                string format = new string(reader.ReadChars(4));
+                if (format != "WAVE")
+                    throw new NotSupportedException("Specified stream is not a wave file.");
+
+                // WAVE header
+                string format_signature = new string(reader.ReadChars(4));
+                if (format_signature != "fmt ")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int format_chunk_size = reader.ReadInt32();
+                int audio_format = reader.ReadInt16();
+                int num_channels = reader.ReadInt16();
+                int sample_rate = reader.ReadInt32();
+                int byte_rate = reader.ReadInt32();
+                int block_align = reader.ReadInt16();
+                int bits_per_sample = reader.ReadInt16();
+
+                string data_signature = new string(reader.ReadChars(4));
+                if (data_signature != "data")
+                    throw new NotSupportedException("Specified wave file is not supported.");
+
+                int data_chunk_size = reader.ReadInt32();
+
+                channels = num_channels;
+                bits = bits_per_sample;
+                rate = sample_rate;
+
+                return reader.ReadBytes((int)reader.BaseStream.Length);
+            }
+        }
+
+        public static ALFormat GetSoundFormat(int channels, int bits)
+        {
+            switch (channels)
+            {
+                case 1: return bits == 8 ? ALFormat.Mono8 : ALFormat.Mono16;
+                case 2: return bits == 8 ? ALFormat.Stereo8 : ALFormat.Stereo16;
+                default: throw new NotSupportedException("The specified sound format is not supported.");
+            }
+        }
+
     }
 }
